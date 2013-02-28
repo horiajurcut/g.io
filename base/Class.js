7476925eb55c331b54a452fd7960e912d90ab414
horiajurcut <horia.dragos@gmail.com>
2013-02-28 19:11:11 +0200

    Base Class - basic inheritance in JS

---
+++ base/Class.js
@@ -0,0 +1,60 @@
+var base = base || {};
+
+base.Class = function() {
+	var BaseClass = new Function(),
+	    __LAZY__ = false;
+	
+	BaseClass.$extend = function(members) {
+		var constructor = this,
+		    definition, childMember, parentMember;
+		
+		if (members.__construct) {
+			constructor = _buildMethod(members.__construct, this);
+		}
+		
+		definition = function() {
+			!__LAZY__ && constructor.apply(this, arguments);
+		}
+		
+		for (var name in this) {
+			definition[name] = this[name];
+		}
+		
+		definition.prototype = _lazyInstance(this);
+		definition.prototype.constructor = constructor;
+		
+		for (var name in members) {
+			childMember  = members[name];
+			parentMember = this.prototype[name];
+			
+			definition.prototype[name] = childMember instanceof Function ? _buildMethod(childMember, parentMember) : childMember;
+		}
+		
+		definition.prototype.$static = definition;
+		definition.$extend = this.$extend;
+		
+		return definition;
+	};
+	
+	function _lazyInstance(obj) {
+		__LAZY__ = true;
+		var newInstance = new obj();
+		__LAZY__ = false;
+		
+		return newInstance;
+	}
+	
+	function _buildMethod(childMember, parentMember) {
+		return function() {
+			var superMember = this.$super;
+			
+			this.$super = parentMember;
+			returnValue = childMember.apply(this, arguments);
+			this.$super = superMember;
+				
+			return returnValue;
+		};
+	}
+	
+	return BaseClass;
+}()
