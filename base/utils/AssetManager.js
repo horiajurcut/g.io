3ba63111695439050937ecd27f6dc79ba785d3d0
horiajurcut <horia.dragos@gmail.com>
2013-03-02 01:24:13 +0200

    Beta AssetManager that loads Images()

---
+++ base/utils/AssetManager.js
@@ -0,0 +1,48 @@
+var base = base || {},
+    base.utils = base.utils || {};
+    
+base.utils.AssetManager = function() {
+	this.loadCount = 0;
+	this.errorCount = 0;
+	
+	this.cache = {};
+	this.imageQueue = [];
+}
+
+base.utils.AssetManager.prototype.pushAsset = function(path) {
+	this.imageQueue.push(path);
+}
+
+base.utils.AssetManager.prototype.downloadAll = function(callback) {
+	var self = this
+	    path = null,
+	    img = null;
+	
+	if (this.imageQueue.length === 0) callback();
+	
+	for (var i = 0; i < this.imageQueue.length; i++) {
+		var path = this.imageQueue[i],
+		    img = new Image();
+		    
+		img.addEventListener('load', function() {
+			++this.loadCount;
+			if (self.completed()) callback();
+		}, false);
+		
+		img.addEventListener('error', function() {
+			++this.errorCount;
+			if (self.completed()) callback();
+		}, false);
+		
+		img.src = path;
+		this.cache[path] = img;
+	}
+}
+
+base.utils.AssetManager.prototype.completed = function() {
+	return (this.imageQueue == this.loadCount + this.errorCount);
+}
+
+base.utils.AssetManager.prototype.getAsset = function(path) {
+	return this.cache[path];
+}
