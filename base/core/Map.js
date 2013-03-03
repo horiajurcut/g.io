96293aa0080f70ca4e1c49cf66724068e2e620f8
horiajurcut <horia.dragos@gmail.com>
2013-03-03 22:49:26 +0200

    Created Engine and Map objects

---
+++ base/core/Map.js
@@ -0,0 +1,12 @@
+var base = base || {};
+    base.core = base.core || {};
+    
+base.core.Map = base.Class.$extend({
+	__construct: function() {
+		
+	}
+});
+
+base.core.Map.prototype.draw = function() {
+	
+}
---
+++ base/core/TileMap.js
@@ -0,0 +1,8 @@
+var base = base || {};
+    base.core = base.core || {};
+    
+base.core.TileMap = base.core.Map.$extend({
+	__construct: function() {
+		
+	}
+});
